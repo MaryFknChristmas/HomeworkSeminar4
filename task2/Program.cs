9f984d2f4c4ad8e164a5b535f93f4464ec5b2dd6
Marina <gtellier.mecanics@gmail.com>
2022-07-08 20:03:28 +0300

    Сумма цифр в числе

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Task2();
+void Task2()
+{
+    Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Во введенном числе сумма цифр равна " + Summa(number));
+}
+
+int Summa(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        sum = sum + number%10;
+        number = number/10;
+    }
+    return sum;
+}
