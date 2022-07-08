4cdd5a4beaee55b4d1f9fd6c07ce2f7954fc57da
Marina <gtellier.mecanics@gmail.com>
2022-07-08 19:48:16 +0300

    Исправлено2

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,11 +1,15 @@
-﻿Console.WriteLine("Введите первое число");
-int numberA = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int numberB = Convert.ToInt32(Console.ReadLine());
-int result = 1;
-
-for(int i = 0; i < numberB; i++)
+﻿void Task1()
 {
-    result = result * numberA;
+    Console.WriteLine("Введите первое число");
+    int numberA = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число");
+    int numberB = Convert.ToInt32(Console.ReadLine());
+    int result = 1;
+
+    for(int i = 0; i < numberB; i++)
+    {
+        result = result * numberA;
+    }
+    Console.WriteLine($"Число {numberA} в степени {numberB} равно {result}");
 }
-Console.WriteLine($"Число {numberA} в степени {numberB} равно {result}");
+Task1();
