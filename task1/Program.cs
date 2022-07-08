238d5030ffbda113598efbca51383b3135ff3733
Marina <gtellier.mecanics@gmail.com>
2022-07-08 19:10:42 +0300

    Преобразование в степень

---
+++ task1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = Convert.ToInt32(Math.Pow(numberA, numberB));
+Console.WriteLine(result);
